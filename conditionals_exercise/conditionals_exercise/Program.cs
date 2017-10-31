2e6d2c653c67092a9ee1b03deb889dc959bbe98d
Ross Scarborough <rossscarborough@gmail.com>
2017-10-31 17:01:10 -0400

    Conditionals Exercise 1

---
+++ conditionals_exercise/conditionals_exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionals_exercise
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /*Exercise 1: Ask the user to enter a number. The number should be between 1 to 10.If the user enters a valid number, display "Valid" on the console.Otherwise, display "Invalid".*/
+            var inputNumber = 10f;
+            if (inputNumber >= 1f && inputNumber <= 10f)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            /*Exercise 2: A program which takes two numbers from the console and displays the maximum of the two.*/
+
+
+        }
+    }
+}
