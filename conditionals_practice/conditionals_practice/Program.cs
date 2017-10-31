addfa5c6cc0ebef156146104fab8cc8924fba599
Ross Scarborough <rossscarborough@gmail.com>
2017-10-31 15:58:33 -0400

    added conditionals and refs vals practice

---
+++ conditionals_practice/conditionals_practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionals_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isStarbucksMember = false;
+            float refillPrice;
+            float ncSalesTax = 1.0675f;
+            if (isStarbucksMember)
+            {
+                refillPrice = 0.00f;
+            }
+            else
+            {
+                refillPrice = 0.50f;
+            }
+            //var reRefillPrice = (isStarbucksMember) ? 0.00f : 0.50f;
+            float total = (float)System.Math.Round((refillPrice * ncSalesTax), 2, MidpointRounding.ToEven);
+            Console.WriteLine("Your total for a refill is " + total);
+
+        }
+    }
+}
