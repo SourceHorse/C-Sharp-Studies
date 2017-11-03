62794473c13a436c9ee0242a03c0ae76fd68d15e
Ross Scarborough <rossscarborough@gmail.com>
2017-11-02 23:19:58 -0400

    Added Enums

---
+++ enums_practice/enums_practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enums_practice
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine("The value of Express is " + (int)method);
+            var methodId = 3;
+            Console.WriteLine("The shipping method being used is " + (ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}
