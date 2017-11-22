40fa7ed44a82c65d11dbea38c246d1c0c7317b4e
Ross Scarborough <rossscarborough@gmail.com>
2017-11-22 12:32:39 -0500

    more practice with classes

---
+++ Classes3/Classes3/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes3
+{
+    public class Car
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+        public int MileagePerGallon;
+        public int Speed = 0;
+
+        public void Accelerate(int increment)
+        {
+            Speed = Speed + increment;
+        }
+    }
+}
---
+++ Classes3/Classes3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fusion = new Car();
+            fusion.Make = "Ford";
+            fusion.Model = "Fusion";
+            fusion.Year = 2017;
+            fusion.MileagePerGallon = 41;
+            fusion.Speed = 0;
+            Console.WriteLine(fusion.Speed);
+            while (Console.ReadLine() != "stop")
+            {
+                fusion.Accelerate(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine(fusion.Speed);
+            }
+            
+            
+        }
+    }
+}
