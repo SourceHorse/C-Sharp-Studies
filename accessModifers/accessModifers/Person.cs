74f6226731daf9de371cb6c8e82234e416b72b78
Ross Scarborough <rossscarborough@gmail.com>
2017-11-30 18:43:12 -0500

    Encapsulation

---
+++ accessModifers/accessModifers/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace accessModifers
+{
+    public class Person
+    {
+
+        private DateTime _birthdate;
+        private string _name;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+
+        public void SetName(string name)
+        {
+            if (!String.IsNullOrWhiteSpace(name))
+            {
+                this._name = name;
+            }
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
---
+++ accessModifers/accessModifers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accessModifers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1984, 7, 23));
+            person.SetName("Cronk McDooferson");
+
+            Console.WriteLine(person.GetBirthdate());
+            Console.WriteLine(person.GetName());
+        }
+    }
+}
