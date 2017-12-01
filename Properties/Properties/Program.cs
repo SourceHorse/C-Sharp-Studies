6ac9947dea71a38bc92900d14800afa95017a36a
Ross Scarborough <rossscarborough@gmail.com>
2017-11-30 23:43:41 -0500

    Properties, set/get, private fields without declaring private fields.

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1977, 3, 23));
+            person.Name = "Jordan";
+            person.Username = "Jdawg1337";
+            Console.WriteLine("{0}, AKA {1}, was born on {2} and is {3} years old.", person.Name, person.Username, person.Birthdate, person.Age);
+        }
+    }
+}
