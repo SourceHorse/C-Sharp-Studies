a4d8738638ed9cc41d604a67e00c8295d78e602e
Ross Scarborough <rossscarborough@gmail.com>
2017-12-03 19:04:22 -0500

    Composition practice

--- Composition/Composition/Program.cs
+++ Composition/Composition/Program.cs
@@ -10,6 +10,14 @@ namespace Composition
     {
         static void Main(string[] args)
         {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
         }
     }
-}
+} 
